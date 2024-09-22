6a74686921e1ba372faf31596eda831f4049bed3
melsxmet <mellisaouttrim@gmail.com>
2024-09-21 20:50:57 -0400

    Initial commit

---
+++ src/test/java/FireFoxBrowserAssignment.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxBrowserAssignment {
+        public static void main(String[] args) {
+
+            WebDriver driver=new FirefoxDriver();
+            driver.get("https://www.syntaxprojects.com/");
+            driver.manage().window().maximize();
+            String currentUrl=driver.getCurrentUrl();
+            System.out.println(currentUrl);
+            String title=driver.getTitle();
+            System.out.println(title);
+            driver.close();
+
+        }
+
+}
